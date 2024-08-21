b77881c3867b429a6eb43cbb24eb01cb5dacf7df
raphasampaio <raphaelaraujosampaio@gmail.com>
2024-08-21 15:43:06 -0300

    Initial commit

--- src/macro.jl
+++ src/macro.jl
@@ -1,17 +1,17 @@
 macro serializer_cache(id, ex)
     quote
-            if !isdir(PATH)
-                mkdir(PATH)
-            end
+        if !isdir(PATH)
+            mkdir(PATH)
+        end
 
-            file = joinpath(PATH, string($(esc(id)), ".bin"))
+        file = joinpath(PATH, string($(esc(id)), ".bin"))
 
-            if isfile(file)
-                Serialization.deserialize(file)
-            else
-                data = $(esc(ex))
-                Serialization.serialize(file, data)
-                data
-            end
+        if isfile(file)
+            Serialization.deserialize(file)
+        else
+            data = $(esc(ex))
+            Serialization.serialize(file, data)
+            data
+        end
     end
 end
