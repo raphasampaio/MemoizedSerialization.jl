671e3a246f863bb590779adba425dfc0e3ca7f5c
raphasampaio <raphaelaraujosampaio@gmail.com>
2024-08-27 13:17:20 -0300

    Add set

--- src/MemoizedSerialization.jl
+++ src/MemoizedSerialization.jl
@@ -3,6 +3,7 @@ module MemoizedSerialization
 using Serialization
 
 const CACHE_PATH = Ref{String}()
+const CACHE_SET = Set{String}()
 
 export @memoized_serialization
 
@@ -20,15 +21,18 @@ function clean!()
         rm(CACHE_PATH[], force = true, recursive = true)
     end
     CACHE_PATH[] = mktempdir(; cleanup = true)
+    empty!(CACHE_SET)
     return nothing
 end
 
 macro memoized_serialization(path, key, expr)
     return quote
         file = joinpath($(esc(path)), string($(esc(key)), ".tmp"))
-        if isfile(file)
+
+        if $(esc(key)) in CACHE_SET
             Serialization.deserialize(file)
         else
+            push!(CACHE_SET, $(esc(key)))
             data = $(esc(expr))
             Serialization.serialize(file, data)
             data
@@ -39,6 +43,5 @@ end
 macro memoized_serialization(key, expr)
     return :(@memoized_serialization(CACHE_PATH[], $(esc(key)), $(esc(expr))))
 end
-
 
 end
