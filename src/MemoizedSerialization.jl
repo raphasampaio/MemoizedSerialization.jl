e412ec9e5ae6bf86643ddef1bdb4b364b1dfc822
raphasampaio <raphaelaraujosampaio@gmail.com>
2024-09-05 18:10:01 -0300

    Project updates

--- src/MemoizedSerialization.jl
+++ src/MemoizedSerialization.jl
@@ -4,6 +4,7 @@ using Serialization
 
 const CACHE_PATH = Ref{String}()
 const CACHE_SET = Set{String}()
+const VERBOSE = Ref{Bool}(false)
 
 export @memoized_serialization
 
@@ -14,6 +15,11 @@ end
 
 function path()
     return CACHE_PATH[]
+end
+
+function verbose(value::Bool)
+    VERBOSE[] = value
+    return nothing
 end
 
 function clean!()
@@ -30,8 +36,14 @@ macro memoized_serialization(path, key, expr)
         file = joinpath($(esc(path)), string($(esc(key)), ".tmp"))
 
         if $(esc(key)) in CACHE_SET
+            if VERBOSE[]
+                println("Loading from cache: ", $(esc(key)))
+            end
             Serialization.deserialize(file)
         else
+            if VERBOSE[]
+                println("Saving to cache: ", $(esc(key)))
+            end
             push!(CACHE_SET, $(esc(key)))
             data = $(esc(expr))
             Serialization.serialize(file, data)
